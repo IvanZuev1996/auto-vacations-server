e54cb37be36477bc381ae12422954f246b80a794
Ivan Zuev <ivan-zuev-97@mail.ru>
2023-10-16 21:23:04 +0300

    fix index.tsx

--- dist/src/index.js
+++ dist/src/index.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import router from './routes';
+import router from './routes/index';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
